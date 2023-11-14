47c150aba85088f498b767193dbc80ff3fbc2b4c
GokhanGKHN <gokhan.namli@outlook.com>
2023-11-14 17:25:33 +0300

    first commit

---
+++ Python Pizza Deliveries.py
@@ -0,0 +1,69 @@
+"""Instructions
+Congratulations, you've got a job at Python Pizza! Your first job is to build an automatic pizza order program.
+
+Based on a user's order, work out their final bill.
+
+Small pizza (S): $15
+
+Medium pizza (M): $20
+
+Large pizza (L): $25
+
+Add pepperoni for small pizza (Y or N): +$2
+
+Add pepperoni for medium or large pizza (Y or N): +$3
+
+Add extra cheese for any size pizza (Y or N): +$1
+
+Example Input
+L
+Y
+N
+Example Output
+Thank you for choosing Python Pizza Deliveries!
+Your final bill is: $28."""
+
+print("Thank you for choosing Python Pizza Deliveries!")
+print("Size: ")
+size = input()
+print("pepperoni(Y/N): ")
+add_pepperoni = input()
+print("cheese (Y/N): ")
+extra_cheese = input()
+bill=0
+
+if size =="S":
+    bill+=15
+elif size=="M":
+    bill+=20
+else :
+    bill+=25
+
+if add_pepperoni =="Y":
+    if size =="S":
+        bill+=2
+    else:
+        bill+=3
+
+        if extra_cheese =="Y" :
+            bill+=1
+
+print(f"Your final bill is: ${bill}.")
+
+
+
+
+
+
+
+
+
+        
+
+
+
+       
+
+
+
+
